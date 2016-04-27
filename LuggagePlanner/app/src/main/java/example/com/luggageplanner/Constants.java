2b1c46583b9bc0e69ab850a0e5dbae351dc47a62
swsaloni <salonimathur15@gmail.com>
2016-04-27 23:43:38 +0530

    Added Images for weather conditions

--- LuggagePlanner/app/src/main/java/example/com/luggageplanner/Constants.java
+++ LuggagePlanner/app/src/main/java/example/com/luggageplanner/Constants.java
@@ -9,7 +9,15 @@ import java.util.Map;
 public class Constants {
 
     public static final Map<String, Integer> iconMap = new HashMap<String, Integer>(){{
-        put("partly-cloudy-night",R.mipmap.ic_launcher);
+        put("clear-day", R.drawable.clear_day);
+        put("rain", R.drawable.rain);
+        put("snow", R.mipmap.ic_launcher);
+        put("sleet", R.mipmap.ic_launcher);
+        put("wind", R.mipmap.ic_launcher);
+        put("fog", R.mipmap.ic_launcher);
+        put("cloudy", R.mipmap.ic_launcher);
+        put("partly-cloudy-day", R.mipmap.ic_launcher);
+        put("partly-cloudy-night", R.mipmap.ic_launcher);
 
     }};
 
