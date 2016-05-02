574db3d8f963d0de287565751b1fcd2a568f1d12
swsaloni <salonimathur15@gmail.com>
2016-05-02 11:03:16 +0530

    Recycler view added

--- LuggagePlanner/app/src/main/java/example/com/luggageplanner/WelcomeActivity.java
+++ LuggagePlanner/app/src/main/java/example/com/luggageplanner/WelcomeActivity.java
@@ -7,8 +7,13 @@ import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationManager;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -19,6 +24,10 @@ import com.android.volley.Response.ErrorListener;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -27,17 +36,26 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Locale;
 
-public class WelcomeActivity extends AppCompatActivity {
+public class WelcomeActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener{
     ImageView imageView;
     TextView myLocation,condition,temperature;
     LocationManager locationManager;
     double lat, longi;
     String apiKey="cc8d0147336eb6d1dd595389e98a93ed";
+    protected GoogleApiClient mgoogleapiclient;
+    private static final LatLngBounds myBounds = new LatLngBounds(new LatLng(-0,0), new LatLng(0,0));
+    EditText searchWidget;
+    RecyclerView myRecycler;
+    LinearLayoutManager myLinearlayoutmanager;
+    MyAdapter myadapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        searchWidget=(EditText)findViewById(R.id.searchWidget);
+        myadapter= new MyAdapter(this,R.layout.search_widget,mgoogleapiclient,myBounds,null);
         imageView=(ImageView)findViewById(R.id.tempImage);
         myLocation =(TextView)findViewById(R.id.location);
         condition=(TextView)findViewById(R.id.condition);
@@ -100,6 +118,21 @@ public class WelcomeActivity extends AppCompatActivity {
        JsonObjectRequest currentWeather = new JsonObjectRequest(Method.GET, "https://api.forecast.io/forecast/" + apiKey + "/" + lat + "," + longi,"",listener,errorListener);
         Volley.newRequestQueue(this).add(currentWeather);
 
+
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
     }
 }
